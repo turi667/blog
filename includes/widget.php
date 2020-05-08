eab5b1b0878edb7be8bef1bf946b6d341beaa53b
turi667 <61826945+turi667@users.noreply.github.com>
2020-05-08 10:27:34 -0700

    Add files via upload

---
+++ includes/widget.php
@@ -0,0 +1,8 @@
+<div class="well">
+                    <h4>About Automania</h4>
+                    <p>If you search for the meaning of community, you’ll find the definition as people who share common interests. That’s why the universe has cooking clubs, football fanatics and Game Of Thrones diehards. But to us petrolheads, cars are so much more than a simply defined ‘interest’. We are defined by a passion that infiltratres our lives on a much deeper level.
+
+And so the stronger the passion, the tighter a community will be, and you’ll find this solidarity by the bucketload in the automotive scene. When we’re not out driving our cars, we’re socialising online with fellow car enthusiasts and subconsciously consuming any car facts and figures we can get our minds on There’s no entry requirement if you want to bear the name of ‘car enthusiast’. A 14-year-old teenager from Canada can have the exact same passion for cars as a classic restorer from Croatia, and working on a rugged E36 BMW can be just as much fun as cleaning the leather seats on a brand new 911. Thanks to the internet, people with different backgrounds can connect and share their passion with each other. This could be on a lobby in Forza Horizon, an owners’ group on Facebook or even a platform like Car Throttle.
+
+Socialising with people from different corners of the globe makes us all more open-minded and teaches us a lot about cultures different from our own. And there aren’t many communities in which this is really true…</p>
+</div>
