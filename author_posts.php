22906c63da996b5f06665443dc7ddd72bde8f3be
turi667 <61826945+turi667@users.noreply.github.com>
2020-05-08 10:46:59 -0700

    Update author_posts.php

--- author_posts.php
+++ author_posts.php
@@ -25,9 +25,23 @@
 
                              }
 
+                 if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin' ) {
 
-    $query = "SELECT * FROM posts WHERE post_user = '{$the_post_user}' ";
+            
+                     $query = "SELECT * FROM posts WHERE post_user = '{$the_post_user}' ";
+                     
 
+
+
+         } else {
+
+
+             $query = "SELECT * FROM posts WHERE post_user = '{$the_post_user}'  AND post_status = 'published' ";
+
+           
+         }
+
+   
     $select_all_posts_query = mysqli_query($connection,$query);
 
     while($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -65,54 +79,6 @@
 
 
 
-  <?php
-
-                if(isset($_POST['create_comment'])) {
-
-                $the_post_id = $_GET['p_id'];
-                $comment_user = $_POST['comment_user'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
-
-
-                if(!empty($comment_user) && !empty($comment_email) && !empty($comment_content) ) {
-
-
-                $query = "INSERT INTO comments (comment_post_id, comment_user, comment_email, comment_content, comment_status,comment_date)";
-
-                $query .= "VALUES ($the_post_id ,'{$comment_user}', '{$comment_email}', '{$comment_content }', 'unapproved',now())";
-
-                $create_comment_query = mysqli_query($connection,$query);
-
-                if(!$create_comment_query ){
-
-                    die('QUERY FAILED' . mysqli_error($connection));
-
-                    }
-
-
-
-                $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-
-                $query .= "WHERE post_id = $the_post_id ";
-
-                $update_comment_count = mysqli_query($connection,$query);
-
-
-
-
-               } else {
-
-              echo "<script>alert('Fields cannot be empty')</script>";
-                       }
-
-
-
-                    }
-
-
-
-?>
 
             </div>
 
