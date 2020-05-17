06219e3ac31822e0afd1ee87f8bba57849087c1b
turi667 <61826945+turi667@users.noreply.github.com>
2020-05-17 06:53:48 -0700

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -35,7 +35,7 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="/cms/search" method="post">
+                    <form action="/cms/search" method="get">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
